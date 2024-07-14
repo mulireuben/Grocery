a33bc910462634cb72660898adad4872661da547
Reuben Muli <mulimumo36@gmail.com>
2024-07-14 14:00:12 +0300

    add cdn,css and icons for the header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Layout } from 'antd';
+import AppHeader from './components/common/Header';
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Layout>
+        <Header>
+          <AppHeader />
+        </Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
     </div>
   );
 }
---
+++ src/components/common/Header.js
@@ -0,0 +1,65 @@
+import {
+  FacebookFilled,
+  InstagramFilled,
+  MailOutlined,
+  MobileOutlined,
+  TwitterSquareFilled,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Button } from 'antd';
+
+function AppHeader() {
+  return (
+    <div className='container'>
+      <div className='topBar'>
+        <div className='contactInfo'>
+          <ul>
+            <li>
+              {' '}
+              <a href='tel:0708377276'>
+                <span>
+                  <MobileOutlined />
+                  0708377276
+                </span>
+              </a>
+            </li>
+            <li>
+              {' '}
+              <a href='mailto:mulireuben57@gmail.com'>
+                <span>
+                  {' '}
+                  <MailOutlined /> mulireuben57@gmail.com
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className='otherInfo'>
+          <ul className='socialMedia'>
+            <li>
+              <a href='https://www.facebook.com'>
+                <FacebookFilled />
+              </a>
+            </li>
+            <li>
+              <a href='https://www.twitter.com'>
+                <TwitterSquareFilled />
+              </a>
+            </li>
+            <li>
+              <a href='https://www.instagram.com'>
+                <InstagramFilled />
+              </a>
+            </li>
+          </ul>
+          <Button>
+            <UserOutlined />
+            My Account
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AppHeader;
